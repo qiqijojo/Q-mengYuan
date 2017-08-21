fa1ced0e122ffe219a5ff253442658bbf54b043d
qiqijojo <774948439@qq.com>
2017-08-21 15:56:24 +0800

    兴趣页面静态实现

--- pages/modal/modal.js
+++ pages/modal/modal.js
@@ -15,72 +15,72 @@ Page({
     expect: ''
   },
   //事件处理函数
-  backIndex: function () {
-    var that = this;
-    wx.request({
-      url: 'http://172.18.33.2/api/user/signup',
-      data:{
-        anonymity:'',
-        loginCode: that.data.loginCode,
-        gender: that.data.gender,
-        nature: that.data.nature,
-        expect: that.data.expect
-      },
-      method:'POST',
-      success:function(res){
-       if(res.data.code === 0){
-         wx.showToast({
-           title: '注册成功',
-           icon:'success',
-           mask:true,
-           success:function(){
-              wx.switchTab({
-                url: '../index/index',
-              })
-           }
-         })
-       } else if (res.data.code === -1){
-         wx.showModal({
-           title: '注册失败！',
-           content: '请检查输入是否正确！',
-           showCancel:false,
-         })
-       }
-      }
-    })
-  },
-  bindMyCharacter:function(e){
-    this.setData({
-      nature: e.detail.value
-    })
-  },
-  bindPeerCharacter:function(e){
-    this.setData({
-      expect: e.detail.value
-    })
-  },
+  // backIndex: function () {
+  //   var that = this;
+  //   wx.request({
+  //     url: 'http://172.18.33.2/api/user/signup',
+  //     data:{
+  //       anonymity:'',
+  //       loginCode: that.data.loginCode,
+  //       gender: that.data.gender,
+  //       nature: that.data.nature,
+  //       expect: that.data.expect
+  //     },
+  //     method:'POST',
+  //     success:function(res){
+  //      if(res.data.code === 0){
+  //        wx.showToast({
+  //          title: '注册成功',
+  //          icon:'success',
+  //          mask:true,
+  //          success:function(){
+  //             wx.switchTab({
+  //               url: '../index/index',
+  //             })
+  //          }
+  //        })
+  //      } else if (res.data.code === -1){
+  //        wx.showModal({
+  //          title: '注册失败！',
+  //          content: '请检查输入是否正确！',
+  //          showCancel:false,
+  //        })
+  //      }
+  //     }
+  //   })
+  // },
+  // bindMyCharacter:function(e){
+  //   this.setData({
+  //     nature: e.detail.value
+  //   })
+  // },
+  // bindPeerCharacter:function(e){
+  //   this.setData({
+  //     expect: e.detail.value
+  //   })
+  // },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (query) {
-    var that = this;
-    that.setData({
-      anonymity: query.userName,
-      loginCode: query.loginCode
-    });
-    console.log(that.data.userName)
-    console.log(that.data.loginCode)
-    app.getUserInfo(function (info){
-      console.log(info)
-      that.setData({
-          sex: info.gender
-      });
-       if (that.data.sex === 1){
-          that.setData({gender:'MAIL'})
-        } else if (that.data.sex === 2){
-            that.setData({gender:'FEMAIL'})
-          }
-    });
+    // var that = this;
+    // that.setData({
+    //   anonymity: query.userName,
+    //   loginCode: query.loginCode
+    // });
+    // console.log(that.data.userName)
+    // console.log(that.data.loginCode)
+    // app.getUserInfo(function (info){
+    //   console.log(info)
+    //   that.setData({
+    //       sex: info.gender
+    //   });
+    //    if (that.data.sex === 1){
+    //       that.setData({gender:'MAIL'})
+    //     } else if (that.data.sex === 2){
+    //         that.setData({gender:'FEMAIL'})
+    //       }
+    // });
   },
 
   /**
