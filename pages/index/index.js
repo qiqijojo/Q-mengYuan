ca477544f0e5a138a06f9ffa2e5e0a1b28e58ea4
qiqijojo <774948439@qq.com>
2017-08-28 13:01:42 +0800

    删除轮播与扫码

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,15 +9,6 @@ Page({
     perData:[],
     pageIndex:0,
     openId:'',
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ],
-    indicatorDots: false,
-    autoplay: true,
-    interval: 5000,
-    duration: 1000
    },
   //事件处理函数
  
