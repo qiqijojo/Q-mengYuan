8d2dfe8b49426f6b5d3e43b6fcfee2d9d5947ccb
qiqijojo <774948439@qq.com>
2017-09-09 20:45:56 +0800

    modify

--- app.js
+++ app.js
@@ -1,10 +1,50 @@
 // app.js
 App({
+  // 启动时登录
   onLaunch: function () {
-    // 调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('user') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('user', logs)
+    var that = this
+
+    // 获取loginCode
+    wx.login({
+      success: (loginRes) => {
+        if (loginRes.code) {
+          // loginCode换取sessionId和userId
+          wx.request({
+            url: that.globalData.mengyuanIp + '/api/user/auth',
+            data: {
+              'loginCode': loginRes.code
+            },
+            method: 'POST',
+            success: (res) => {
+              let authRes = res.data
+              that.globalData.sessionId = authRes.data.sessionId
+              that.globalData.userId = authRes.data.userId
+
+              // 将userId存储到缓存
+              try {
+                wx.setStorageSync('userId', `${authRes.data.userId}`)
+              } catch (err) {
+                console.error('设置userId失败')
+                throw new Error(err)
+              }
+              // 将sessionId存储到缓存
+              try {
+                wx.setStorageSync('sessionId', `${authRes.data.sessionId}`)
+              } catch (err) {
+                console.error('设置sessionId失败')
+                throw new Error(err)
+              }
+
+              // 获取用户信息
+              that.getUserInfo()
+            }
+          })
+        } else {
+          console.error('获取用户登录态失败!')
+          throw new Error(loginRes)
+        }
+      }
+    })
   },
 
   getUserInfo: function (cb) {
@@ -23,13 +63,13 @@ App({
     }
   },
   globalData: {
-    users: [],
     userId: '',
     userInfo: {},
     labelWarp: [],
     labelCount: [],
     customInput: '',
-    // mengyuanIp: 'http://172.19.25.44'
-    mengyuanIp: 'https://hackathon-yuan.dearitgirl.com'
+    // mengyuanIp: 'http://172.18.33.4',
+    mengyuanIp: 'https://hackathon-yuan.dearitgirl.com',
+    users: []
   }
 })
--- pages/modal/modal.js
+++ pages/modal/modal.js
@@ -21,7 +21,8 @@ Page({
     allLabel: [],
     labelWarp: [],
     labelArr: [],
-    lightColor: '' // 颜色高亮
+    lightColor: '', // 颜色高亮
+    textColor:''
   },
   // 事件处理函数
 
@@ -185,7 +186,8 @@ Page({
         selfLabel[index]['flag'] = index
         app.globalData.labelWarp.push(selfLabel[index])
         app.globalData.labelCount.push(index)
-        selfLabel[i].lightColor = '#1AAD19'
+        selfLabel[i].lightColor = '#ee5a52'
+        selfLabel[i].textColor = '#fff'
       }
     }
     this.setData({
@@ -207,6 +209,7 @@ Page({
       selfLabel.map((ele, k) => {
         if (k == flag) {
           ele.lightColor = '#F8F8F8'
+          ele.textColor = '#333'
         }
       })
       this.setData({
